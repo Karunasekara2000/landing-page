cd7bbbc24204e5ed20f1dbad92da64879096f2c6
lahiruSandaruwan20 <lahiru.20210390@iit.ac.lk>
2025-07-31 12:00:17 +0530

    project initialise.

---
+++ landing-page/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { SplashComponent } from './components/splash/splash.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingComponent,
+    SplashComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
