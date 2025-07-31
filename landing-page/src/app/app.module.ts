e3120029c17848fb10bba4a92a6fcff85e1aaf16
lahiruSandaruwan20 <lahiru.20210390@iit.ac.lk>
2025-07-31 16:30:48 +0530

    implemented the register.

--- landing-page/src/app/app.module.ts
+++ landing-page/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { SplashComponent } from './components/splash/splash.component';
+import {HttpClientModule} from "@angular/common/http";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { SplashComponent } from './components/splash/splash.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- landing-page/src/app/landing/landing.component.ts
+++ landing-page/src/app/landing/landing.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthService, RegisterRequest} from "../services/auth.service";
 
 @Component({
   selector: 'app-landing',
@@ -7,17 +9,42 @@ import { Component } from '@angular/core';
 })
 export class LandingComponent {
 
-  speakers = [
-    {
-      name: 'Dr. Khalid Al Khatib',
-      title: 'Director, AML Department, SAMA',
-      img: 'assets/speaker1.jpg'
-    },
-    {
-      name: 'Sarah Al-Farhan',
-      title: 'Head of Compliance, Riyadh Bank',
-      img: 'assets/speaker2.jpg'
-    },
-    // Add more speakers as needed
-  ];
+
+
+  registerForm: FormGroup;
+  loading = false;
+  successMsg = '';
+  errorMsg = '';
+
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.registerForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    this.successMsg = '';
+    this.errorMsg = '';
+    if (this.registerForm.invalid) return;
+
+    this.loading = true;
+    const payload: RegisterRequest = this.registerForm.value;
+
+    this.authService.register(payload).subscribe({
+      next: res => {
+        this.successMsg = 'Registration successful!';
+        this.registerForm.reset();
+        this.loading = false;
+      },
+      error: err => {
+        this.errorMsg = err?.error?.message || 'Registration failed. Please try again.';
+        this.loading = false;
+      }
+    });
+  }
+
+
 }
