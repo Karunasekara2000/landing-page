032feec12e5856113a8f1a19b71e8894e945a69f
lahiruSandaruwan20 <lahiru.20210390@iit.ac.lk>
2025-07-31 16:07:28 +0530

    implemented landing page.

--- landing-page/src/app/app-routing.module.ts
+++ landing-page/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SplashComponent} from "./components/splash/splash.component";
+import {LandingComponent} from "./landing/landing.component";
 
 const routes: Routes = [
+  { path: '', component: SplashComponent },  // default: splash screen
+  { path: 'home', component: LandingComponent },  // actual landing page
 ];
 
 @NgModule({
--- landing-page/src/app/landing/landing.component.ts
+++ landing-page/src/app/landing/landing.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class LandingComponent {
 
+  speakers = [
+    {
+      name: 'Dr. Khalid Al Khatib',
+      title: 'Director, AML Department, SAMA',
+      img: 'assets/speaker1.jpg'
+    },
+    {
+      name: 'Sarah Al-Farhan',
+      title: 'Head of Compliance, Riyadh Bank',
+      img: 'assets/speaker2.jpg'
+    },
+    // Add more speakers as needed
+  ];
 }
